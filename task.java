2ac6229d72ba86cbf9cd1d4289cd10950e88fec6
Jowits <jowita.lapies@gmail.com>
2020-03-20 17:04:16 +0000

    new task

---
+++ task.java
@@ -0,0 +1,13 @@
+package helloWorld;
+
+public class NewHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		     System.out.println("Hello, World"); 
+		    
+
+	}
+
+}
